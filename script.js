189aae44e729bc96339dc026c4aa13a6c98eaf43
tanmaysarin <tanmaysarin@gmail.com>
2022-07-02 13:47:55 -0700

    partial dice roll functionality added

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+// selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// initializing game to starting point
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3.Check for rolled 1
+  if (dice !== 1) {
+    // add dice to parent score
+    currentScore = currentScore + dice;
+    current0El.textContent = currentScore; //CHANGE LATER
+  } else {
+    //set current score to 0
+    currentScore = 0;
+    current0El.textContent = currentScore;
+    // switch to the next player
+  }
+});
